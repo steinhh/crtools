81cfc4d216760e9a8742e5d801e211eaeae66d83
steinhh <s.v.h.haugan@astro.uio.no>
2025-11-09 17:19:29 +0100

    Further sorting network expansions

--- src/ftools/sorting/test_sorting_c.c
+++ src/ftools/sorting/test_sorting_c.c
@@ -84,7 +84,28 @@ int main()
 
   all_passed &= test_sorting_network(sort3, 3, "sort3");
   all_passed &= test_sorting_network(sort4, 4, "sort4");
+  all_passed &= test_sorting_network(sort5, 5, "sort5");
+  all_passed &= test_sorting_network(sort6, 6, "sort6");
+  all_passed &= test_sorting_network(sort7, 7, "sort7");
+  all_passed &= test_sorting_network(sort8, 8, "sort8");
   all_passed &= test_sorting_network(sort9, 9, "sort9");
+  all_passed &= test_sorting_network(sort11, 11, "sort11");
+  all_passed &= test_sorting_network(sort12, 12, "sort12");
+  all_passed &= test_sorting_network(sort13, 13, "sort13");
+  all_passed &= test_sorting_network(sort14, 14, "sort14");
+  all_passed &= test_sorting_network(sort15, 15, "sort15");
+  all_passed &= test_sorting_network(sort16, 16, "sort16");
+  all_passed &= test_sorting_network(sort17, 17, "sort17");
+  all_passed &= test_sorting_network(sort18, 18, "sort18");
+  all_passed &= test_sorting_network(sort19, 19, "sort19");
+  all_passed &= test_sorting_network(sort20, 20, "sort20");
+  all_passed &= test_sorting_network(sort21, 21, "sort21");
+  all_passed &= test_sorting_network(sort22, 22, "sort22");
+  all_passed &= test_sorting_network(sort23, 23, "sort23");
+  all_passed &= test_sorting_network(sort24, 24, "sort24");
+  all_passed &= test_sorting_network(sort25, 25, "sort25 (hybrid)");
+  all_passed &= test_sorting_network(sort25b, 25, "sort25b (complete network)");
+  all_passed &= test_sorting_network(sort26, 26, "sort26");
   all_passed &= test_sorting_network(sort27, 27, "sort27 (hybrid)");
   all_passed &= test_sorting_network(sort27b, 27, "sort27b (complete network)");
 
