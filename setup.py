541a9cf2c0ac7897a3869895b62492fb984ac9df
steinhh <s.v.h.haugan@astro.uio.no>
2025-11-10 15:55:40 +0100

    Fix setup.py version number

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ ext_modules = [
 
 setup(
     name="ftools",
-    version="3.2.2-main",
+    version="3.2.3",
     description="Small C extensions for local image filters (fmedian, fsigma)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
