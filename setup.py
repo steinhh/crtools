0c9963be53734a7a36261b7b88dab74fe840e5e0
steinhh <s.v.h.haugan@astro.uio.no>
2025-11-10 16:01:47 +0100

    Remove duplicated sort124/sort125

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ ext_modules = [
 
 setup(
     name="ftools",
-    version="3.2.3",
+    version="3.2.4",
     description="Small C extensions for local image filters (fmedian, fsigma)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
