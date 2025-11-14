4960ba53b18fecd201e74ad2ed0937bc0a74685c
steinhh <s.v.h.haugan@astro.uio.no>
2025-11-14 17:15:43 +0100

    Access C functions directly in tests

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ ext_modules = [
 
 setup(
     name="ftools",
-    version="4.0.11",
+    version="4.0.12",
     description="Small C extensions for local image filters (fmedian, fsigma)",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
